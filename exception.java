2b8b1fb382015f61a9d2307cbe754c481a6ee7cb
aejas389 <23hr1a3252@mtieat.org>
2024-12-14 15:17:29 +0530

    Add files via upload

---
+++ exception.java
@@ -0,0 +1,10 @@
+public class exception{
+    public static void main(String[] args) {
+        try{
+            throw new Exception("Error");
+        }catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
